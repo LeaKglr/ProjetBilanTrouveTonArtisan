6b687abf5a3c8480e7fd3a543ee7a31fec136250
LéaKugler <leakugler2@gmail.com>
2024-07-31 19:19:37 +0200

    Modification page fiche artisans

--- src/app/fiche-artisan/fiche-artisan.component.ts
+++ src/app/fiche-artisan/fiche-artisan.component.ts
@@ -1,10 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Artisans, ArtisansService } from '../artisans.service';
+import { ActivatedRoute } from '@angular/router';
+import { firstValueFrom } from 'rxjs';
+import { Subscription } from 'rxjs';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-fiche-artisan',
   templateUrl: './fiche-artisan.component.html',
   styleUrl: './fiche-artisan.component.scss'
 })
-export class FicheArtisanComponent {
+export class FicheArtisanComponent implements OnInit {
+  artisan: Artisans | undefined;
+  private routeSub: Subscription | null = null;
+  isSubmitted = false;
+  hasError = false;
+  
 
+  constructor (
+    private route: ActivatedRoute, 
+    private artisansService: ArtisansService) {
+  }
+
+  onSubmit(form: NgForm) {
+    if (form.valid) {
+      this.isSubmitted = true;
+      this.hasError = false;
+      form.resetForm();
+    } else {
+      this.hasError = true;
+      this.displayValidationErrors(form);
+    }
+  }
+
+  displayValidationErrors(form: NgForm) {
+    Object.keys(form.controls).forEach(key => {
+      const controlErrors = form.controls[key].errors;
+      if (controlErrors) {
+        console.log(`Key control: ${key}, errors:`, controlErrors);
+      }
+    });
+  }
+
+  ngOnInit(): void {
+    this.routeSub = this.route.paramMap.subscribe(async params => {
+      const name = params.get('name');
+      if (name) {
+        this.artisan = await firstValueFrom(this.artisansService.getArtisanByName(name));
+      }
+    });
+  }
+
+  getStars(note:number): boolean[] {
+    return Array(5).fill(false).map((_,i) => i < note);
+  }
 }
